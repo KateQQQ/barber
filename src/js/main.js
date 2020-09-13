665570ce4bd39c4911c6678a0ebff574fe5982ef
Oleksandr Kurinnyi <exzos@iMac-Oleksandr.local>
2020-09-14 00:15:53 +0300

    update index-page

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
-/*
- * Third party
- */
-//= ../../bower_components/jquery/dist/jquery.js
+//= ../../node_modules/@iconfu/svg-inject/dist/svg-inject
 
-
-/*
- * Custom
- */
-//= partials/app.js
+var mySwiper = new Swiper(".index-gallery", {
+  // loop: true,
+  navigation: {
+    nextEl: ".index-gallery__next",
+    prevEl: ".index-gallery__prev",
+  },
+});
