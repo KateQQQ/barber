185cb686e6e7b35f319825986eeac07dd4ace343
KateQQQ <alexander.kr.dev@gmail.com>
2020-09-18 19:14:50 +0300

    modal pages creation

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,12 @@ var mySwiper = new Swiper(".index-gallery", {
     prevEl: ".index-gallery__prev",
   },
 });
+
+function mapModalOpen() {
+  const mapModal = document.querySelector("#map");
+  return mapModal.classList.toggle("visually-hidden");
+}
+function loginModalOpen() {
+  const loginModal = document.querySelector("#login");
+  return loginModal.classList.toggle("visually-hidden");
+}
