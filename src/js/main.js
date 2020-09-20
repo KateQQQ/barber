1679f27afd4914684f819b70300569352310240c
KateQQQ <alexander.kr.dev@gmail.com>
2020-09-20 18:53:03 +0300

    final changes

--- src/js/main.js
+++ src/js/main.js
@@ -36,3 +36,22 @@ function initMenuActivePage() {
 // https://swiperjs.com/demos/
 
 initMenuActivePage();
+
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
